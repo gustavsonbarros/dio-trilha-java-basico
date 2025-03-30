96a619f8ec9ff5aad852ecf1e3ee170c250932ac
gustavsonbarros <gustavson.adm@gmail.com>
2025-03-30 12:37:08 -0300

    Desafio concluido

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) {
-        System.out.println("Ola");
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Por favor! Digite o numero da Conta");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Digite o numero da Agencia");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite o numero do Cliente");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o saldo");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("\nOla" + nomeCliente + ", Obrigado por criar a conta em nosso banco, "
+        + "Sua Agencia " + agencia + ", conta " + numero + "Saldo R$" + saldo + "Disponivel para saque");
+
+        scanner.close();
     }
 }
